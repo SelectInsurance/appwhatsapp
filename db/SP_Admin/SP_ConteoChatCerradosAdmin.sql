f028e9802f2e840cdf050bc9a4a7a3b5140ad994
Selectinsurance <Selectinsurance210@gmail.com>
2021-10-14 14:36:10 -0500

    Agregando StoreProcedure SP_ConteoChatAbiertosAdmin y SP_ConteoChatCerradosADmin

--- db/SP_Admin/SP_ConteoChatCerradosAdmin.sql
+++ db/SP_Admin/SP_ConteoChatCerradosAdmin.sql
@@ -2,16 +2,15 @@ USE whatsapp;
 
 DROP PROCEDURE IF EXISTS SP_ConteoChatCerradosAdmin;
 DELIMITER //
-CREATE procedure SP_ConteoChatCerradosAdmin(in v_usuario VARCHAR(255))
+CREATE PROCEDURE SP_ConteoChatCerradosAdmin(in v_usuario VARCHAR(255))
 BEGIN
 
+DECLARE v_id INT;
 DECLARE v_conteo INT;
-DECLARE v_idAgentes INT;
-SET v_idAgentes = (SELECT id FROM Agentes WHERE creador = v_usuario);
-SET v_conteo = (SELECT COUNT(abierto) FROM  dialogs WHERE idAgentes = v_idAgentes);
-
-SELECT v_conteo;
-
+SET v_id = (SELECT id FROM Agentes WHERE usuario = v_usuario);
+SET v_conteo = (SELECT count(abierto) FROM dialogs WHERE idAgentes = v_id AND abierto = FALSE);
+select  v_conteo;
 END //
+
 DELIMITER;
 
--- db/database.sql
+++ db/database.sql
@@ -66,6 +66,22 @@
   );
 
 
+DROP PROCEDURE IF EXISTS SP_ConteoChatAbiertosAdmin;
+DELIMITER //
+CREATE PROCEDURE SP_ConteoChatAbiertosAdmin(in v_usuario VARCHAR(255))
+BEGIN
+
+DECLARE v_id INT;
+DECLARE v_conteo INT;
+SET v_id = (SELECT id FROM Agentes WHERE usuario = v_usuario);
+SET v_conteo = (SELECT count(abierto) FROM dialogs WHERE idAgentes = v_id);
+select  v_conteo;
+END //
+
+
+
+
+
 
 
 
