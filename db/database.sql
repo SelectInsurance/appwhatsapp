037d4171b85e07859141c97bd15783fe0d4a5fea
Selectinsurance <Selectinsurance210@gmail.com>
2021-10-20 10:47:55 -0500

    Agregando procedimiento almacenado SP_ReadACcesWebToken

--- db/database.sql
+++ db/database.sql
@@ -224,6 +224,22 @@ END //
 
 
 
+DROP Procedure IF EXISTS SP_ReadAccesWebToken;
+
+DELIMITER //
+CREATE Procedure SP_ReadAccesWebToken(IN v_user VARCHAR(55))
+BEGIN
+
+DECLARE v_creador VARCHAR(255);
+SET v_creador = (SELECT creador FROM Agentes WHERE usuario = v_user);
+
+SELECT * FROM TokenChatApi WHERE user = v_creador;
+END //
+
+
+
+
+
 
 
 
