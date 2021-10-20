3dfd543a49a03143c3b7f3cc844183e28dc2f29d
Selectinsurance <Selectinsurance210@gmail.com>
2021-10-20 09:36:36 -0500

    Agregando nav y conector al usuario Asistente

--- app/asistant/controller/controller.php
+++ app/asistant/controller/controller.php
@@ -1,6 +1,6 @@
 <?php
 
-
+require_once 'app\asistant\models\app_autoload.php';
 
 //Funciones para requerir encabezado, pie de pagina y menu
 function higher()
@@ -10,6 +10,32 @@ function higher()
 
 function Nav()
 {
+    $user = $_SESSION['Asistant'];
+    //Recibiendo Salas de chat abiertas desde la app de whatsapp
+    $AwebT = mysqli_fetch_assoc(crud::Read(query::ReadAwebT($user)));
+    $ChatApi = new ChatApi($AwebT['Instance'], $AwebT['Token']);
+    $array = $ChatApi->Dialogs();
+
+    //var_dump($array);
+
+    //logica para sacar cantidad de indices y recorrer el array con la cantidad de indices
+    foreach ($array as $key => $value) {
+        $j = count($value);
+        $i = 0;
+
+        while ($i < $j) {
+            crud::Create(query::CreateDialogs($value[$i]['id'], $value[$i]['name'], $value[$i]['image'], $value[$i]['last_time']));
+            $i++;
+        }
+    }
+
+    $user = $_SESSION['Asistant'];
+    //Salas de chat almacenadas en base de datos
+    $consulta = crud::Read(query::ReadDialogs($user));
+
+
+
+
     require_once 'app\asistant\views\assets\Nav.phtml';
 }
 
---
+++ app/asistant/views/assets/js/funciones.js
@@ -0,0 +1,3 @@
+$(document).ready(function () {
+    console.log('Hola desde Jquery usando rol Asistente');
+});
