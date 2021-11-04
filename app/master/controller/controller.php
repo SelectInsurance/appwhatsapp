52daa9e4923249717c0e9ed5208fb1ad1329474b
dextter1913 <dextter1913@gmail.com>
2021-11-04 11:07:19 -0500

    Agregando validacion de nuevo

--- app/master/controller/controller.php
+++ app/master/controller/controller.php
@@ -60,9 +60,8 @@ class controller
         if (isset($AwebT['Instance']) && isset($AwebT['Token'])) {
             $ChatApi = new ChatApi($AwebT['Instance'], $AwebT['Token']);
             $json = $ChatApi->Dialogs();
-            if (!isset($_POST['filtrarNav'])) {
                 print $json;
-            } elseif ($_POST['filtrarNav'] != '') {
+            if ($_POST['filtrarNav'] != '') {
                 $filtrarNav = $_POST['filtrarNav'];
 
                 //Salas de chat almacenadas en base de datos
